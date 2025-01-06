0bf981c3480def0015afc725db33807eb3dbd1eb
Lareine-26 <lareinbadaan@gmail.com>
2025-01-06 17:31:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
+
 df = pd.read_csv('parkinsons.csv')
 df.head()
 x = df[['MDVP:Fo(Hz)', 'MDVP:Flo(Hz)']]
 y = df['status']
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(strategy='mean')  
 x = imputer.fit_transform(x)
 
 # Scale the data
 scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
+from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+from sklearn.neighbors import KNeighborsClassifier
 
 # Choose a KNN model
 model = KNeighborsClassifier(n_neighbors=5)
